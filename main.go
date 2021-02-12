36ee2d6e8bc633c3f63ef6e21a94454317b076f5
us <rf.rvr@yandex.ru>
2021-02-12 15:23:01 +0300

    bug list update

--- main.go
+++ main.go
@@ -6,13 +6,9 @@ import (
 
 //messenger in golang
 //https://github.com/easmith/p2p-messenger
-//https://stackoverflow.com/questions/11886531/terminating-a-process-started-with-os-exec-in-golang
-func main(){
 
-	//defer func() {
-	//
-	//		logggerScan.SaveToLog(string(debug.Stack()))
-	//	}()
+func main(){
+	println("app start")
 	mainWindow.StartUI()
 }
 
