14333b929e68feee43525874686af9ec8cc0aa6f
us <rf.rvr@yandex.ru>
2021-02-16 09:23:04 +0300

    fix win pack

--- include/unarchive/Win_comContext.go
+++ include/unarchive/Win_comContext.go
@@ -3,14 +3,13 @@
 package unarchive
 
 import (
-	"os/exec"
 	"context"
-	"syscall"
+	"os/exec"
 )
 
 func getCommandContext(ctx context.Context,path,dest string)*exec.Cmd{
 	var cmd *exec.Cmd
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	cmd = exec.CommandContext(ctx,pass7zWindows,"e",path,"-o"+dest)
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	return cmd
 }
