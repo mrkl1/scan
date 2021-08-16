a607692bc7191450b7c7b83fdfd54214cb587ff0
us <rf.rvr@yandex.ru>
2021-08-16 09:51:44 +0300

    Rename search error

--- include/unarchive/zipRenameWithEncoding.go
+++ include/unarchive/zipRenameWithEncoding.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
 	"unicode/utf8"
 )
 
@@ -20,14 +21,20 @@ WARNING
 chcp1251/866, KOI-8R и тд
  */
 func RenameZipIncorrectName(zipPath string){
-	fi,_ := ioutil.ReadDir(zipPath)
+	fi,err := ioutil.ReadDir(zipPath)
+	if err != nil {
+		logggerScan.SaveToLog("RenameZipIncorrectName err:"+err.Error())
+	}
 	logggerScan.SaveToLog("******"+zipPath)
 	for _,v := range fi {
+
+		logggerScan.SaveToLog(zipPath+"+"+v.Name()+"+"+strconv.FormatBool(utf8.ValidString(v.Name()))+"+"+cpd.CodepageAutoDetect([]byte(v.Name())).String())
 		if !utf8.ValidString(v.Name()){
 			logggerScan.SaveToLog("incorrect zip")
 			cp := cpd.CodepageAutoDetect([]byte(v.Name()))
 			text,_ := charmap.ANY_to_UTF8( []byte(v.Name()), cp.String())
 			logggerScan.SaveToLog("****** "+string(text)+" (name)")
+			os.Rename(filepath.Join(zipPath,v.Name())  ,filepath.Join(zipPath,string(text)))
 			os.Rename(filepath.Join(zipPath,v.Name())  ,filepath.Join(zipPath,string(text)))
 		}
 	}
